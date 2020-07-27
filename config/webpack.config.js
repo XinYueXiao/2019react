9f9900899a50973788df26a9c56e2a701593c197
wangxinyue <wangxinyue@weiboyi>
2020-07-27 16:44:41 +0800

    测试less

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -316,14 +316,43 @@ module.exports = function (webpackEnv) {
           include: paths.appSrc,
         },
         {
-          test: /\.less$/,
-          use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-          }, {
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "less-loader" // compiles Less to CSS
-          }]
+          test: /\.(less|css)$/,
+          use: [
+            require.resolve('style-loader'),
+            {
+              loader: require.resolve('css-loader'),
+              options: {
+                importLoaders: 1,
+              },
+            },
+
+            {
+              loader: require.resolve('postcss-loader'),
+              options: {
+                // Necessary for external CSS imports to work
+                // https://github.com/facebookincubator/create-react-app/issues/2677
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9', // React doesn't support IE8 anyway
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            },
+            {
+              loader: require.resolve('less-loader'),
+              options: {
+                javascriptEnabled: true
+              }
+            },
+          ],
         },
         {
           // "oneOf" will traverse all following loaders until one will
